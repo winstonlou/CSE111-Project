42cbc8156c4e77088167764f880327374114f530
syang113 <116114122+syang113@users.noreply.github.com>
2022-11-08 17:04:33 -0800

    Add files via upload

--- project.py
+++ project.py
@@ -35,49 +35,49 @@ def createTables(_conn):
     _conn.execute("BEGIN")
     try:
         sql = """CREATE TABLE date (
-                    da_id decimal (9,0) NOT NULL,
+                    da_id decimal (9,0),
                     da_mid decimal (9,0) NOT NULL,
                     da_releaseyear Integer NOT NULL)"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE duration (
-                    du_id decimal (9,0) NOT NULL,
+                    du_id decimal (9,0),
                     du_mid decimal (9,0) NOT NULL,
                     du_runtime Integer NOT NULL)"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE movies (
-                    m_mid decimal (9,0) NOT NULL,
+                    m_mid decimal (9,0),
                     m_gid decimal (9,0) NOT NULL,
                     m_title varchar(100) NOT NULL)"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE genre (
-                    g_gid decimal (9,0) NOT NULL,
+                    g_gid decimal (9,0),
                     g_gname char (25) NOT NULL)"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE ratings (
-                    ra_id decimal (9,0) NOT NULL,
+                    ra_id decimal (9,0),
                     ra_mid decimal (9,0) NOT NULL,
                     ra_ratings char(10) NOT NULL)"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE reviews (
-                    re_id decimal (9,0) NOT NULL,
+                    re_id decimal (9,0),
                     re_imbdid decimal (9,0) NOT NULL,
                     re_review decimal (9,0) NOT NULL)"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE director (
-                    di_id decimal (9,0) NOT NULL,
+                    di_id decimal (9,0),
                     di_mid decimal (9,0) NOT NULL,
                     di_name varchar (50) NOT NULL,
                     di_name2 varchar (50))"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE user (
-                    u_id decimal (9,0) NOT NULL,
+                    u_id decimal (9,0),
                     u_mid decimal (9,0) NOT NULL,
                     u_watching Integer NOT NULL,
                     u_watched Integer NOT NULL,
@@ -91,12 +91,68 @@ def createTables(_conn):
         _conn.execute(sql)
 
         _conn.execute("COMMIT")
-        print("successfully created table warehouse")
+        print("successfully created tables")
     except Error as e:
         _conn.execute("ROLLBACK")
         print(e)
 
     print("++++++++++++++++++++++++++++++++++")
+
+def createtest(_conn):
+    print("++++++++++++++++++++++++++++++++++")
+
+    try:
+        
+        values = [
+            (1, "{1},{3},{2} ", 'Toy Story'),
+            (2," ", 'Four Rooms'),
+            (3," ", 'Jumanji'),
+            (4," ", 'Fargo'),
+            (5," ", "Miller's Crossing"),
+            (6," ", 'The Big Lebowski'),
+            (7," ", 'Phantoms'),
+            (8," ", 'Hellraiser:Bloodline'),
+            (9," ", ''),
+        ]
+
+
+        sql = "INSERT INTO movie VALUES(?, ?)"
+        _conn.executemany(sql, values)
+
+        values = [
+            (1,'Animation'),
+            (2,'Comedy'),
+            (3,'Family'),
+            (4,'Adventure'),
+            (5,"Fantasy"),
+            (6,'Crime'),
+            (7,'Drama'),
+            (8,'Thriller'),
+            (9,'Action'),
+            (10,'Horror')
+            (11,'Science Fiction'),
+        ]
+
+
+        sql = "INSERT INTO genre VALUES(?, ?)"
+        _conn.executemany(sql, values)
+
+
+
+
+
+
+
+
+
+        _conn.execute("COMMIT")
+        print("successfully inputed test values")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+
 
 
 def main():
