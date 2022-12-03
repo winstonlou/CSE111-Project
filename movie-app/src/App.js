7e9c59ba0cf9a6406311fa80c7bf4097bf94d6ec
Winston Lou <75230116+winstonlou@users.noreply.github.com>
2022-12-02 16:58:37 -0800

    template for navigation bar

---
+++ movie-app/src/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './App.css';
+  
+function App() {
+    return (
+        <div>
+            <nav class="navbar background">
+                <ul class="nav-list">
+                    <div class="title">
+                        <h1>Movie Recommendation</h1>
+                    </div>
+                    <li><a href="#watching-list">Current List</a></li>
+                    <li><a href="#watched-list">Past List</a></li>
+                    <li><a href="#to-watch-list">Future List</a></li>
+                    <li><a href='#filter'>Filter</a></li>
+                </ul>
+  
+                <div class="rightNav">
+                    <input type="text" name="search" id="search" />
+                    <button class="btn btn-sm">Search</button>
+                </div>
+            </nav>
+  
+            <section class="section">
+                <div class="box-main">
+                    <div class="firstHalf">
+                        <h1 class="text-big">
+                            
+                        </h1>
+                        <p class="text-small">
+                           
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                           
+                        </h1>
+                        <p class="text-small">
+                            
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            
+                        </h1>
+                        <p class="text-small">
+                            
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            
+                        </h1>
+                        <p class="text-small">
+                            
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <footer className="footer">
+                <p className="text-footer">
+                    
+                </p>
+            </footer>
+        </div>
+    )
+}
+  
+export default App
