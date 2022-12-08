87b0c81f99b11419902ef91157469fa79eaec0db
Winston Lou <75230116+winstonlou@users.noreply.github.com>
2022-12-07 20:48:23 -0800

    update

--- final/Page.py
+++ final/Page.py
@@ -33,6 +33,58 @@ def closeConnection(_conn):
         print(e)
 
     print("++++++++++++++++++++++++++++++++++")
+def displaySearch(results):
+    conn = openConnection()
+    cur = conn.cursor()
+    print(results)
+    datas =[]
+    for result in results:
+        result = str(result)
+        result = result.strip("(").strip(")").strip(",").strip("'")
+        datas.append(result)
+    print(datas)
+   
+    finals = []
+    for data in datas:
+        totals = []
+        final =[]
+        print(data)
+        sql = f"""SELECT m_title, ad_ratings, da_releaseyear, du_runtime FROM movies,dates,duration,adult 
+        WHERE m_adid = ad_adid AND m_duid = du_duid AND m_daid = da_daid AND m_title ="{data}"; """
+        cur.execute(sql)
+        values = cur.fetchall()
+        sql = f"""SELECT total.di_diname AS D1 
+        FROM (
+        SELECT movies.m_mid, movies.m_title, director.di_diname FROM movies
+        INNER JOIN director ON movie_director.md_diid = director.di_diid
+        INNER JOIN movie_director ON movies.m_mid = movie_director.md_mid) AS total 
+        WHERE total.m_title ="{data}";"""
+        cur.execute(sql)
+        values2 = cur.fetchall()
+        sql = f"""SELECT g_gname FROM genre
+        INNER JOIN movies ON movie_genre.mg_mid = movies.m_mid
+        INNER JOIN movie_genre ON genre.g_gid = movie_genre.mg_gid
+        WHERE m_title = "{data}";"""
+        cur.execute(sql)
+        values3 = cur.fetchall()
+
+        #print(values)
+        #print(values2)
+        #print(values3)
+        totals += values + values2 + values3
+        #print(total)
+        counts = 0
+        for total in totals:
+            total = str(total)
+            total = total.strip("(").strip(")").strip(",").strip("'")
+            final.append(total)
+            counts = counts + 1
+        finals += final
+    print(finals)
+    closeConnection(conn)
+    return finals
+
+
 
 @app.route('/')
 def index():
@@ -50,7 +102,17 @@ def getMovieList():
     closeConnection(conn)
     
     return json.dumps(results)
-    
+
+@app.route('/nameSearch/<string:name>', methods = ["GET"])
+def getGenre(name):
+    conn = openConnection()
+    cur = conn.cursor()
+    sql = f"""SELECT m_title FROM movies WHERE m_title LIKE '{name}%';"""
+    cur.execute(sql)
+    results = cur.fetchall()
+    closeConnection(conn)    
+    return results
+
 
 
 if __name__ == '__main__':
--- final/static/Page.js
+++ final/static/Page.js
@@ -12,7 +12,55 @@ async function filter() {
     })
     .then(response =>response.json())
     .then(data => DisplayData(data))
+}
+
+function Search(){
+  var name = document.getElementById("movie-name").value
+  var genre1 = document.getElementById("dropdown").value
+  var genre2 = document.getElementById("dropdown2").value
+  var genre3 = document.getElementById("dropdown3").value
+
+  console.log(name)
+  console.log(genre1)
+  console.log(genre2)
+  console.log(genre3)
+
+  if (name =="" && genre1 == "None" && genre2 == "None" && genre3 == "None"){
+    console.log("Nothing")
+  } else if(name =="" && genre1 == "None" && genre2 == "None" && genre3 != "None"){
+    console.log("1 Genre, Genre3")
+  } else if(name =="" && genre1 == "None" && genre2 != "None" && genre3 == "None"){
+    console.log("1 Genre, Genre2")
+  } else if(name =="" && genre1 != "None" && genre2 == "None" && genre3 == "None"){
+    console.log("1 Genre, Genre1")
+  } else if(name =="" && genre1 == "None" && genre2 != "None" && genre3 != "None"){
+    console.log("2 Genre, Genre 3 and 2")
+  } else if(name =="" && genre1 != "None" && genre2 != "None" && genre3 == "None"){
+    console.log("2 Genre, Genre 1 and 2")
+  } else if(name =="" && genre1 != "None" && genre2 == "None" && genre3 != "None"){
+    console.log("2 Genre, Genre 1 and 3")
+  } else if(name =="" && genre1 != "None" && genre2 != "None" && genre3 != "None"){
+    console.log("3 Genre")
+  } else if (name !="" && genre1 == "None" && genre2 == "None" && genre3 == "None"){
+    console.log("Name only")
+    nameSearch()
+  } else if(name !="" && genre1 == "None" && genre2 == "None" && genre3 != "None"){
+    console.log("Name and Genre3")
+  } else if(name !="" && genre1 == "None" && genre2 != "None" && genre3 == "None"){
+    console.log("Name and Genre2")
+  } else if(name !="" && genre1 != "None" && genre2 == "None" && genre3 == "None"){
+    console.log("Name and Genre1")
+  } else if(name !="" && genre1 == "None" && genre2 != "None" && genre3 != "None"){
+    console.log("Name + Genre 3 and 2")
+  } else if(name !="" && genre1 != "None" && genre2 != "None" && genre3 == "None"){
+    console.log("Name + Genre 1 and 2")
+  } else if(name !="" && genre1 != "None" && genre2 == "None" && genre3 != "None"){
+    console.log("Name + Genre 1 and 3")
+  } else if(name !="" && genre1 != "None" && genre2 != "None" && genre3 != "None"){
+    console.log("Name + 3 Genres")
   }
+}
+
 
 function DisplayData(data){
   display = document.getElementById("display")
@@ -25,6 +73,19 @@ function DisplayData(data){
     entry +="<br>"+ String(data[item]) + "<br>"
   }
   document.getElementById("display").innerHTML = entry;
+}
+
+
+async function nameSearch(){
+  var name = document.getElementById("movie-name").value
+  console.log("nameSearch")
+  const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url + "nameSearch/"+ name, true);
+    xhttp.onload = function(){
+      document.getElementById("display").innerHTML = this.responseText
+    };
+    
+    xhttp.send(name);
 }
 
 //DROPDOWN BUTTON
