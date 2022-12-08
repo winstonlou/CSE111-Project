7cbb3ee7f4de647a6f3943ed65b3a0c7c360ed12
syang113 <116114122+syang113@users.noreply.github.com>
2022-12-07 22:21:02 -0800

    Add files via upload

--- final/Page.py
+++ final/Page.py
@@ -33,6 +33,58 @@ def closeConnection(_conn):
         print(e)
 
     print("++++++++++++++++++++++++++++++++++")
+def displaySearch(results):
+    conn = openConnection()
+    cur = conn.cursor()
+    print(results)
+    datas =[]
+    for result in results:
+        result = str(result)
+        result = result.strip("(").strip(")").strip(",").strip("'")
+        datas.append(result)
+    print(datas)
+   
+    finals = []
+    for data in datas:
+        totals = []
+        final =[]
+        print(data)
+        sql = f"""SELECT m_title, ad_ratings, da_releaseyear, du_runtime FROM movies,dates,duration,adult 
+        WHERE m_adid = ad_adid AND m_duid = du_duid AND m_daid = da_daid AND m_title ="{data}"; """
+        cur.execute(sql)
+        values = cur.fetchall()
+        sql = f"""SELECT total.di_diname AS D1 
+        FROM (
+        SELECT movies.m_mid, movies.m_title, director.di_diname FROM movies
+        INNER JOIN director ON movie_director.md_diid = director.di_diid
+        INNER JOIN movie_director ON movies.m_mid = movie_director.md_mid) AS total 
+        WHERE total.m_title ="{data}";"""
+        cur.execute(sql)
+        values2 = cur.fetchall()
+        sql = f"""SELECT g_gname FROM genre
+        INNER JOIN movies ON movie_genre.mg_mid = movies.m_mid
+        INNER JOIN movie_genre ON genre.g_gid = movie_genre.mg_gid
+        WHERE m_title = "{data}";"""
+        cur.execute(sql)
+        values3 = cur.fetchall()
+
+        #print(values)
+        #print(values2)
+        #print(values3)
+        totals += values + values2 + values3
+        #print(total)
+        counts = 0
+        for total in totals:
+            total = str(total)
+            total = total.strip("(").strip(")").strip(",").strip("'")
+            final.append(total)
+            counts = counts + 1
+        finals += final
+    print(finals)
+    closeConnection(conn)
+    return finals
+
+
 
 @app.route('/')
 def index():
@@ -50,7 +102,130 @@ def getMovieList():
     closeConnection(conn)
     
     return json.dumps(results)
-    
+
+@app.route('/nameSearch/<string:name>', methods = ["GET"])
+def getGenre(name):
+    conn = openConnection()
+    cur = conn.cursor()
+    sql = f"""SELECT m_title, ad_ratings, da_releaseyear, du_runtime, g_gname FROM movies,adult,dates,duration,genre,movie_genre 
+    WHERE m_adid = ad_adid AND m_duid = du_duid AND m_daid = da_daid AND movie_genre.mg_mid = movies.m_mid AND
+    genre.g_gid = movie_genre.mg_gid AND m_title LIKE '%{name}%';"""
+    cur.execute(sql)
+    results = cur.fetchall()
+    closeConnection(conn)
+    return json.dumps(results)
+
+@app.route('/NaG1/<string:name>/<string:genre>', methods = ["GET"])
+def getNameAndGenre1(name,genre):
+    conn = openConnection()
+    cur = conn.cursor()
+    cur.execute(f"""SELECT m_title FROM movies, movie_genre, genre
+        WHERE movies.m_mid = movie_genre.mg_mid AND genre.g_gid = movie_genre.mg_gid AND 
+        m_title LIKE '%{name}%' AND g_gname = "{genre}";""")
+    results = cur.fetchall()
+    #print(results)
+    data =[]
+    for result in results:
+        #print(result)
+        result = str(result)
+        result = result.strip("(").strip(")").strip(",").strip("'")
+        #print(result)
+        sql = f"""SELECT m_title, ad_ratings, da_releaseyear, du_runtime, g_gname FROM movies,adult,dates,duration,genre,movie_genre 
+        WHERE m_adid = ad_adid AND m_duid = du_duid AND m_daid = da_daid AND movie_genre.mg_mid = movies.m_mid AND
+        genre.g_gid = movie_genre.mg_gid AND m_title = "{result}";"""
+        cur.execute(sql)
+        results = cur.fetchall()
+        #print(results)
+        data += results
+    #print(data)
+    closeConnection(conn)
+    return json.dumps(data)
+
+@app.route('/G1/<string:genre>', methods = ["GET"])
+def Genre1(genre):
+    conn = openConnection()
+    cur = conn.cursor()
+    cur.execute(f"""SELECT movies.m_title FROM movies, movie_genre, genre
+        WHERE movies.m_mid = movie_genre.mg_mid AND genre.g_gid = movie_genre.mg_gid AND genre.g_gname = ?
+        """,[genre])
+    results = cur.fetchall()
+    #print(results)
+    data =[]
+    for result in results:
+        #print(result)
+        result = str(result)
+        result = result.strip("(").strip(")").strip(",").strip("'").strip('"')
+        #print(result)
+        sql = f"""SELECT m_title, ad_ratings, da_releaseyear, du_runtime, g_gname FROM movies,adult,dates,duration,genre,movie_genre 
+        WHERE m_adid = ad_adid AND m_duid = du_duid AND m_daid = da_daid AND movie_genre.mg_mid = movies.m_mid AND
+        genre.g_gid = movie_genre.mg_gid AND m_title = "{result}";"""
+        cur.execute(sql)
+        results = cur.fetchall()
+        print(results)
+        data += results
+    #print(data)
+    closeConnection(conn)
+    return json.dumps(data)
+
+@app.route('/NaG2/<string:name>/<string:genre>/<string:genre2>', methods = ["GET"])
+def getNameAndGenre2(name,genre,genre2):
+    conn = openConnection()
+    cur = conn.cursor()
+    cur.execute(f"""SELECT m_title FROM movies, movie_genre, genre
+        WHERE movies.m_mid = movie_genre.mg_mid AND genre.g_gid = movie_genre.mg_gid AND m_title LIKE '%{name}%' AND g_gname ='{genre}'
+        INTERSECT
+        SELECT m_title FROM movies, movie_genre, genre
+        WHERE movies.m_mid = movie_genre.mg_mid AND genre.g_gid = movie_genre.mg_gid AND m_title LIKE '%{name}%' AND g_gname = '{genre2}'
+        """)
+    results = cur.fetchall()
+    print(results)
+    data =[]
+    for result in results:
+        print(result)
+        result = str(result)
+        result = result.strip("(").strip(")").strip(",").strip("'")
+        print(result)
+        sql = f"""SELECT m_title, ad_ratings, da_releaseyear, du_runtime, g_gname FROM movies,adult,dates,duration,genre,movie_genre 
+        WHERE m_adid = ad_adid AND m_duid = du_duid AND m_daid = da_daid AND movie_genre.mg_mid = movies.m_mid AND
+        genre.g_gid = movie_genre.mg_gid AND m_title = '{result}';"""
+        cur.execute(sql)
+        results = cur.fetchall()
+        print(results)
+        data += results
+    print(data)
+    closeConnection(conn)
+    return json.dumps(data)
+
+@app.route('/NaG3/<string:name>/<string:genre>/<string:genre2>/<string:genre3>', methods = ["GET"])
+def getNameAndGenre3(name,genre,genre2,genre3):
+    conn = openConnection()
+    cur = conn.cursor()
+    cur.execute(f"""SELECT m_title FROM movies, movie_genre, genre
+        WHERE movies.m_mid = movie_genre.mg_mid AND genre.g_gid = movie_genre.mg_gid AND m_title LIKE '%{name}%' AND g_gname ='{genre}'
+        INTERSECT
+        SELECT m_title FROM movies, movie_genre, genre
+        WHERE movies.m_mid = movie_genre.mg_mid AND genre.g_gid = movie_genre.mg_gid AND m_title LIKE '%{name}%' AND g_gname = '{genre2}'
+        INTERSECT
+        SELECT m_title FROM movies, movie_genre, genre
+        WHERE movies.m_mid = movie_genre.mg_mid AND genre.g_gid = movie_genre.mg_gid AND m_title LIKE '%{name}%' AND g_gname = '{genre3}'""")
+    results = cur.fetchall()
+    print(results)
+    data =[]
+    for result in results:
+        print(result)
+        result = str(result)
+        result = result.strip("(").strip(")").strip(",").strip("'")
+        print(result)
+        sql = f"""SELECT m_title, ad_ratings, da_releaseyear, du_runtime, g_gname FROM movies,adult,dates,duration,genre,movie_genre 
+        WHERE m_adid = ad_adid AND m_duid = du_duid AND m_daid = da_daid AND movie_genre.mg_mid = movies.m_mid AND
+        genre.g_gid = movie_genre.mg_gid AND m_title = '{result}';"""
+        cur.execute(sql)
+        results = cur.fetchall()
+        print(results)
+        data += results
+    print(data)
+    closeConnection(conn)
+    return json.dumps(data)
 
 
 if __name__ == '__main__':
--- final/static/Page.js
+++ final/static/Page.js
@@ -11,8 +11,67 @@ async function filter() {
       
     })
     .then(response =>response.json())
+    .then(data => console.log(data))
     .then(data => DisplayData(data))
+}
+
+function Search(){
+  var name = document.getElementById("movie-name").value
+  var genre1 = document.getElementById("dropdown").value
+  var genre2 = document.getElementById("dropdown2").value
+  var genre3 = document.getElementById("dropdown3").value
+
+  console.log(name)
+  console.log(genre1)
+  console.log(genre2)
+  console.log(genre3)
+
+  if (name =="" && genre1 == "None" && genre2 == "None" && genre3 == "None"){
+    console.log("Nothing")
+  } else if(name =="" && genre1 == "None" && genre2 == "None" && genre3 != "None"){
+    console.log("1 Genre, Genre3")
+    Genre1(genre3)
+  } else if(name =="" && genre1 == "None" && genre2 != "None" && genre3 == "None"){
+    console.log("1 Genre, Genre2")
+    Genre1(genre2)
+  } else if(name =="" && genre1 != "None" && genre2 == "None" && genre3 == "None"){
+    console.log("1 Genre, Genre1")
+    Genre1(genre1)
+  } else if(name =="" && genre1 == "None" && genre2 != "None" && genre3 != "None"){
+    console.log("2 Genre, Genre 3 and 2")
+  } else if(name =="" && genre1 != "None" && genre2 != "None" && genre3 == "None"){
+    console.log("2 Genre, Genre 1 and 2")
+  } else if(name =="" && genre1 != "None" && genre2 == "None" && genre3 != "None"){
+    console.log("2 Genre, Genre 1 and 3")
+  } else if(name =="" && genre1 != "None" && genre2 != "None" && genre3 != "None"){
+    console.log("3 Genre")
+  } else if (name !="" && genre1 == "None" && genre2 == "None" && genre3 == "None"){
+    console.log("Name only")
+    nameSearch()
+  } else if(name !="" && genre1 == "None" && genre2 == "None" && genre3 != "None"){
+    console.log("Name and Genre3")
+    nameAndGenre1(genre3)
+  } else if(name !="" && genre1 == "None" && genre2 != "None" && genre3 == "None"){
+    console.log("Name and Genre2")
+    nameAndGenre1(genre2)
+  } else if(name !="" && genre1 != "None" && genre2 == "None" && genre3 == "None"){
+    console.log("Name and Genre1")
+    nameAndGenre1(genre1)
+  } else if(name !="" && genre1 == "None" && genre2 != "None" && genre3 != "None"){
+    console.log("Name + Genre 3 and 2")
+    nameAndGenre2(genre2,genre3)
+  } else if(name !="" && genre1 != "None" && genre2 != "None" && genre3 == "None"){
+    console.log("Name + Genre 1 and 2")
+    nameAndGenre2(genre1,genre2)
+  } else if(name !="" && genre1 != "None" && genre2 == "None" && genre3 != "None"){
+    console.log("Name + Genre 1 and 3")
+    nameAndGenre2(genre1,genre3)
+  } else if(name !="" && genre1 != "None" && genre2 != "None" && genre3 != "None"){
+    console.log("Name + 3 Genres")
+    nameAndGenre3(genre1,genre2,genre3)
   }
+}
+
 
 function DisplayData(data){
   display = document.getElementById("display")
@@ -20,9 +79,156 @@ function DisplayData(data){
     display.removeChild(display.firstChild);
   }
   var entry = ""
+  entry += "<tr>"
+  entry += "<th>Movie Name</th>"
+  entry += "<th>Adult(18+)</th>"
+  entry += "<th>Release Year</th>"
+  entry += "<th>Duration(mins)</th>"
+  entry += "<th>Genre</th>"
+  entry += "<th>Add Movie</th>"
+  entry += "</tr>"
+  data2 = []
+  filter2 = []
+  count = 0
+  len = data.length
+  console.log(len)
+  for(item in data){
+    small = data[item]
+    if(item > 0 && item != data-1){
+      small2 = data[item-1]
+      if(small[0]==small2[0]){
+        filter2.push(small2[4])
+        small2[4] = ""
+        console.log(filter2)
+      } else {
+        filter2.push(small2[4])
+        small2[4] = ""
+        small2[4] += filter2
+        filter2 =[]
+        console.log(small2)
+      }
+    }
+    if(item == data.length-1){
+      console.log("Final")
+      filter2.push(small[4])
+      small[4] =""
+      small[4] += filter2
+      filter2 = []
+      console.log(small)
+    }
+    console.log(item)
+
+  }
+  console.log(data)
 
   for (item in data){
-    entry +="<br>"+ String(data[item]) + "<br>"
+    small = data[item]
+    if(small[4] == ""){
+
+    }else{
+      data2.push(small)
+    }
+  }
+  console.log(data2)
+
+  for (item in data2){
+    small = data2[item]
+    entry += "<tr>"
+    entry +="<th>"+ small[0] + "<th>"
+    entry +="<th>"+ small[1] + "<th>"
+    entry +="<th>"+ small[2] + "<th>"
+    entry +="<th>"+ small[3] + "<th>"
+    entry +="<th>"+ small[4] + "<th>"
+    entry += "<th>" + "<button onclick=addMovie()>Add Movie</button>" + "</th>"
+    entry += "</tr>"
   }
   document.getElementById("display").innerHTML = entry;
+}
+
+
+async function nameSearch(){
+  var name = document.getElementById("movie-name").value
+  console.log("nameSearch")
+  const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url + "nameSearch/"+ name, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onload = function(){
+
+      data = JSON.parse(this.responseText)
+      console.log(data)
+      DisplayData(data)
+     
+    };
+    
+    xhttp.send(name);
+}
+
+async function nameAndGenre1(genre){
+  var name = document.getElementById("movie-name").value
+
+  console.log("Name and 1 Genre Search")
+  const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url + "NaG1/"+ name + "/"+genre, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onload = function(){
+
+      data = JSON.parse(this.responseText)
+      console.log(data)
+      DisplayData(data)
+     
+    };
+    
+    xhttp.send(name,genre);
+}
+
+async function Genre1(genre){
+  console.log("1 Genre Search")
+  const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url + "G1/"+genre, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onload = function(){
+
+      data = JSON.parse(this.responseText)
+      console.log(data)
+      DisplayData(data)
+     
+    };
+    
+    xhttp.send(genre);
+}
+
+async function nameAndGenre2(genre,genre2){
+  var name = document.getElementById("movie-name").value
+
+  console.log("Name and 2 Genres Search")
+  const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url + "NaG2/"+ name + "/"+genre + "/" + genre2, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onload = function(){
+
+      data = JSON.parse(this.responseText)
+      console.log(data)
+      DisplayData(data)
+     
+    };
+    
+    xhttp.send(name,genre,genre2);
+}
+
+async function nameAndGenre3(genre,genre2,genre3){
+  var name = document.getElementById("movie-name").value
+
+  console.log("Name and 3 Genres Search")
+  const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url + "NaG3/"+ name + "/"+genre + "/" + genre2+ "/" + genre3, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onload = function(){
+
+      data = JSON.parse(this.responseText)
+      console.log(data)
+      DisplayData(data)
+     
+    };
+    
+    xhttp.send(name,genre,genre2,genre3);
 }
