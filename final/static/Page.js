d0236b6175461e60c3330edf7c9ae6e296b84053
Winston Lou <75230116+winstonlou@users.noreply.github.com>
2022-12-07 16:06:48 -0800

    dropdown button

--- final/static/Page.js
+++ final/static/Page.js
@@ -26,3 +26,24 @@ function DisplayData(data){
   }
   document.getElementById("display").innerHTML = entry;
 }
+
+//DROPDOWN BUTTON
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
